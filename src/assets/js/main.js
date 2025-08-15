9adcfb11af7c0e02f552023f0e7946e99cc1637b
EunBi <dmsgod270@gmail.com>
2025-08-15 15:47:37 +0900

    내용 수정

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,3 +9,25 @@ window.addEventListener("load", function () {
   link();
   smooth();
 });
+
+document.querySelectorAll(".site").forEach((button) => {
+  button.addEventListener("click", () => {
+    const modalId = button.getAttribute("data-modal");
+    document.getElementById(modalId).style.display = "block";
+  });
+});
+
+document.querySelectorAll(".close").forEach((closeBtn) => {
+  closeBtn.addEventListener("click", () => {
+    const modalId = closeBtn.getAttribute("data-close");
+    document.getElementById(modalId).style.display = "none";
+  });
+});
+
+window.addEventListener("click", (e) => {
+  document.querySelectorAll(".modal").forEach((modal) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
